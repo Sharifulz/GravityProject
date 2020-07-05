694a73dd8dae9cce1ac39b8239552a7bb09a3c5f
shaarifulz <shaarifulz@gmail.com>
2020-07-05 11:51:20 +0600

    Started learning vue js, API consuming is tested

--- MyHome/src/main/java/com/exam/controller/HomeController.java
+++ MyHome/src/main/java/com/exam/controller/HomeController.java
@@ -3,12 +3,14 @@ package com.exam.controller;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exam.service.IProductService;
 
 @RestController
+@CrossOrigin("*")
 public class HomeController {
 
 	@Autowired
